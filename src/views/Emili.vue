f5813769070a7d58c1dd349fc11b464099a1301f
spricigom <emilispricigo.ifc@gmail.com>
2024-07-23 15:13:42 -0300

    FEAT: Atualização membro Emili #3

--- src/views/Emili.vue
+++ src/views/Emili.vue
@@ -5,24 +5,24 @@
     <header>
         <div>
             <div>
-                <h1></h1>
+                <h1>Emili Spricigo</h1>
                 <p></p>
             </div>
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/Emili">Emili</RouterLink>
-                <RouterLink to="/Francisco">Francisco</RouterLink>
-                <RouterLink to="/Heloisa">Heloisa</RouterLink>
-                <RouterLink to="/Luiz">Luiz Eduardo</RouterLink>
-                <RouterLink to="/Luna">Luna</RouterLink>
-                <RouterLink to="/Sara">Sara</RouterLink>
-
-            </nav>
         </div>
 
     </header>
 
-    <RouterView />
+    <main>
+        <p> Olá, sou a Emili Spricigo e tenho 16 anos! Estudo nbo IFC Campus Araquri e curso Ensino Médio Integrado
+            Tećnico em Informática para Internet.
+            Moro em Joinville e sou da 2INFO3, escolhi este curso pois queria fazer faculdade de Engenharia de Software
+            e sempre amei programar em lego, mudei de ideia ao longo deste tempo mas continuo amando o curso.
+            No momento estou envolida em dois projetos: um sendo de robótica que o Prof° Marco Rojas coordena e o outro
+            sendo secretária do projeto de música da Prof° Jaqueline.
+        </p>
+
+    </main>
+
 </template>
 
 <style scoped></style>
